587e3289129600f2dc0d2b24c55914912a23326d
WertQ <wertqj@gmail.com>
2023-01-25 02:02:29 +0200

    initial commit

---
+++ server.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, request
+import csv
+
+
+app = Flask(__name__)
+
+print(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/<string:page_name>')
+def html_page(page_name):
+    return render_template(page_name)
+
+def write_to_file(data):
+    with open ('database.txt', 'a') as database:
+        email = data["email"]
+        name = data["name"]
+        message = data["message"]
+        database.write(f'\n{email},{name},{message}')
+
+def write_to_csv(data):
+    with open ('database.csv', mode='a', newline='\n') as database2:
+        email = data["email"]
+        name = data["name"]
+        message = data["message"]
+        csv_writer = csv.writer(database2, delimiter=',', quotechar='`', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email, name, message])
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            write_to_csv(data)
+            return render_template('thank_you.html')
+        except:
+            return "could not writeinto database"
+    else:
+        return "something went wrong"
